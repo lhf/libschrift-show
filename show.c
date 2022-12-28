817bf048148def8f54336376ee739485536473f4
lhf <lhf@tecgraf.puc-rio.br>
2022-12-27 23:22:26 -0300

    Add files via upload

--- show.c
+++ show.c
@@ -16,7 +16,7 @@ static const char *progname;
 
 static void fatal(const char *message)
 {
-	fprintf(stderr, "%s: %s\n", progname, message);
+	fprintf(stderr,"%s: %s\n",progname,message);
 	exit(1);
 }
 
@@ -34,8 +34,8 @@ static void loadfont(SFT *sft, const char *filename, double size)
 
 static void loadglyph(const SFT *sft, SFT_UChar codepoint, SFT_Glyph *glyph, SFT_GMetrics *metrics)
 {
-	if (sft_lookup(sft, codepoint, glyph) < 0) fatal("sft_lookup failed");
-	if (sft_gmetrics(sft, *glyph, metrics)< 0) fatal("sft_gmetrics failed");
+	if (sft_lookup(sft,codepoint,glyph) < 0) fatal("sft_lookup failed");
+	if (sft_gmetrics(sft,*glyph,metrics)< 0) fatal("sft_gmetrics failed");
 }
 
 static void newimage(SFT_Image *image, int width, int height, int color)
@@ -60,7 +60,6 @@ static void copyimage(SFT_Image *dest, const SFT_Image *source, int x0, int y0, 
 	unsigned char *d=dest->pixels;
 	unsigned char *s=source->pixels;
 	int y;
-	//for (y=0; y<dest->height; y++) d[x0+y*dest->width]=150;
 	d+=x0+y0*dest->width;
 	for (y=0; y<source->height; y++)
 	{
@@ -91,7 +90,7 @@ int main(int argc, char *argv[])
 	size = atof(argv[2]);
 	message = argv[3];
 
-	loadfont(&sft, filename, size);
+	loadfont(&sft,filename,size);
 
 	size_t k;
 	int margin = 100;
@@ -104,11 +103,11 @@ int main(int argc, char *argv[])
 		SFT_UChar cp = (SFT_UChar) message[k];
 		SFT_Glyph gid;
 		SFT_GMetrics mtx;
-		loadglyph(&sft, cp, &gid, &mtx);
+		loadglyph(&sft,cp,&gid,&mtx);
 		width  += max(mtx.advanceWidth,mtx.minWidth);
 		aheight = max(aheight,-mtx.yOffset);
 		bheight = max(bheight, mtx.yOffset+mtx.minHeight);
-		if (k==0 && mtx.leftSideBearing<0 && margin<-mtx.leftSideBearing) margin=mtx.leftSideBearing;
+		if (k==0 && mtx.leftSideBearing<0 && margin<-mtx.leftSideBearing) margin-=mtx.leftSideBearing;
 	}
 	height = aheight+bheight;
 	width += 2*margin;
@@ -116,9 +115,7 @@ int main(int argc, char *argv[])
 	newimage(&canvas,width,height,255);
 
 	double x = margin;
-	int y = margin;
-	int baseline = aheight;
-	y += baseline;
+	double y = margin+aheight;
 	SFT_Glyph ogid=0;
 	for (k=0; k<strlen(message); k++)
 	{
@@ -127,17 +124,16 @@ int main(int argc, char *argv[])
 		SFT_Glyph gid;
 		SFT_GMetrics mtx;
 		SFT_Kerning kerning;
-		loadglyph(&sft, cp, &gid, &mtx);
-		newimage(&image, mtx.minWidth, mtx.minHeight,0);
-		sft_render(&sft, gid, image);
-		sft_kerning (&sft, ogid, gid, &kerning);
+		loadglyph(&sft,cp,&gid,&mtx);
+		newimage(&image,mtx.minWidth,mtx.minHeight,0);
+		sft_render(&sft,gid,image);
+		sft_kerning(&sft,ogid,gid,&kerning);
 		x += kerning.xShift;
 		copyimage(&canvas,&image,x+mtx.leftSideBearing,y+mtx.yOffset,0);
 		x += mtx.advanceWidth;
 		free(image.pixels);
 		ogid=gid;
 	}
-	//memset((char*)canvas.pixels+y*canvas.width, 128, canvas.width);
 	saveimage(&canvas,stdout);
 
 	free(canvas.pixels);
